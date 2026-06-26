fdd93b861881334e24981ce11f8bd91817280f27
Maks <maks@brzezins.pl>
2026-06-26 21:17:42 +0200

    Initial project commit

---
+++ src/main/java/pl/brzezins/hentselvsenddata/HentselvsenddataApplication.java
@@ -0,0 +1,13 @@
+package pl.brzezins.hentselvsenddata;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class HentselvsenddataApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(HentselvsenddataApplication.class, args);
+	}
+
+}
